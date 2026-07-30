31d58a515f9a3060075f94ac055a588a76aa98a1
Idan Sheizaf <45230634+IdanSheizaf@users.noreply.github.com>
2026-07-30 23:54:59 +0300

    Update 00_index.sh

--- cluster DGE/00_index.sh
+++ cluster DGE/00_index.sh
@@ -1,15 +1,16 @@
+### Everything until CONFIGURATION is specific for HUJI CLUSTER environment
 #!/bin/bash
 #SBATCH --job-name=01_salmon_index
 #SBATCH --cpus-per-task=8
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --time=4:00:00
-#SBATCH --mail-user=idan.slurm@gmail.com
-#SBATCH --output=/sci/labs/ariel.chipman/idansh/scripts/logs/salmon_index_%j.out
-#SBATCH --error=/sci/labs/ariel.chipman/idansh/scripts/errors/salmon_index_%j.err
+#SBATCH --mail-user=user@email.com
+#SBATCH --output=//path/to/log.log
+#SBATCH --error=/path/to/error.err
 #SBATCH --mail-type=FAIL,BEGIN,END
 
-# 2. Environment
+# --- Environment ---
 . /etc/profile.d/huji-lmod.sh
 module load spack salmon
 
